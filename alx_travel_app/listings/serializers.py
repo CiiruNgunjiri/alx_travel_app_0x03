af2b7883ca9ec6e5ba2cc97344c5a61ec453a98e
Linda Ngunjiri <ciiru.ngunjiri@gmail.com>
2025-07-19 23:29:57 +0300

    edited serializers.py

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -4,22 +4,14 @@ from alx_travel_app.listings.models import Listing, Booking, Review
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
-        fields = ['id', 'title', 'description', 'price_per_night', 'host', 'location', 'created_at']
+        fields = '__all__'
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = ['id', 'user', 'listing', 'start_date', 'end_date', 'created_at']
+        fields = '__all__'
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = [
-            'id',
-            'user',
-            'listing',
-            'rating',
-            'comment',
-            'created_at',
-        ]
-        read_only_fields = ['id', 'created_at']
+        fields = '__all__'
